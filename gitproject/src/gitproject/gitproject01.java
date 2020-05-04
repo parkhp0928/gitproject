4f6f1204509ba5657471d318659d12c84551acef
yj-603-11 <yj-603-11@DESKTOP-G500JPD>
2020-05-04 12:44:27 +0900

    git version2

--- gitproject/src/gitproject/gitproject01.java
+++ gitproject/src/gitproject/gitproject01.java
@@ -3,7 +3,8 @@ package gitproject;
 public class gitproject01 {
 
 	public static void main(String[] args) {
-	    System.out.println("Hello World");	
+	    System.out.println("Hello World");
+	    System.out.println("헬로월드");
 	}
 
 }
